7a91a7a2223bb98eabd0ee69caf6158fb0963f0c
Glen Polo <gpolo@cloudbees.com>
2024-07-29 12:23:20 -0400

    overwriting network configs

--- demo/fm_init.py
+++ demo/fm_init.py
@@ -3,6 +3,7 @@ from rox.server.rox_server import Rox
 from rox.server.flags.rox_flag import RoxFlag
 from rox.core.entities.rox_string import RoxString
 from rox.core.entities.rox_int import RoxInt
+from rox.server.rox_options import RoxOptions, NetworkConfigurationsOptions
 
 # Create Roxflags in the Flags container class
 class Flags:
@@ -18,9 +19,25 @@ flags = Flags()
 # Register the flags container
 Rox.register(flags)
 
+# OVERWRITING network configs to point to a customer environment
+# VPC --> overwrite to point to different environment besides Multi-Tenant Production.
+payload = data.get('payload')
+options = payload.get('options')
+configuration = options.get('configuration')
+network_config = NetworkConfigurationsOptions(
+    configuration['https://api.vpc-install-test.saas-tools.beescloud.com/device/get_configuration'],
+    configuration['https://rox-conf.vpc-install-test.saas-tools.beescloud.com'],
+    configuration['https://api.vpc-install-test.saas-tools.beescloud.com/device/update_state_store/'],
+    configuration['https://rox-state.vpc-install-test.saas-tools.beescloud.com'],
+    configuration['https://fm-analytics.vpc-install-test.saas-tools.beescloud.com'],
+    configuration['https://sdk-notification-service.vpc-install-test.saas-tools.beescloud.com/sse'])
+
+# last value is disbableSignatureVerification
+options = RoxOptions(None, network_config, None, True)
+
 # Setup the SDK key
 sdk_key = '<YOUR-SDK-KEY>'
-cancel_event = Rox.setup(sdk_key).result();
+cancel_event = Rox.setup(sdk_key, options).result();
 
 # Boolean flag example
 print('showMessage is {}'.format(flags.showMessage.is_enabled()))
